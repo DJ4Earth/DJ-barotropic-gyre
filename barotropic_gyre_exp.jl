4a74d5b7713acc227af637998e3037c6062d00a3
Sarah Williamson <swilliamson@utexas.edu>
2022-12-12 14:51:58 -0600

    editing code

--- barotropic_gyre_exp.jl
+++ barotropic_gyre_exp.jl
@@ -11,6 +11,7 @@ grid = LatitudeLongitudeGrid(size = (60, 60, 1),
                              z = (-4000, 0))
 
 @inline wind_stress(λ, φ, t, p) = p.τ₀ * cos(2π * (φ - p.φ₀) / p.Lφ)
+
 wind_stress_bc = FluxBoundaryCondition(wind_stress, parameters = (τ₀=1e-4, Lφ=grid.Ly, φ₀=15))
 
 @inline u_bottom_drag(i, j, grid, clock, fields, μ) = @inbounds - μ * fields.u[i, j, 1]
--- debugging.jl
+++ debugging.jl
@@ -17,6 +17,7 @@ grid = LatitudeLongitudeGrid(size = (60, 60, 1),
                              latitude = (15, 75),
                              z = (-4000, 0))
 
+# remove wind stress 
 #@inline wind_stress(λ, φ, t, p) = 0.0 .*( p.τ₀ * cos(2π * (φ - p.φ₀) / p.Lφ) ) 
 # wind_stress_bc = FluxBoundaryCondition(wind_stress, parameters = (τ₀=1e-4, Lφ=grid.Ly, φ₀=15))
 # wind_stress_bc = FluxBoundaryCondition(wind_stress, parameters = (τ₀=0.0, Lφ=0.0, φ₀=0.0))
@@ -25,7 +26,10 @@ grid = LatitudeLongitudeGrid(size = (60, 60, 1),
 @inline u_bottom_drag(i, j, grid, clock, fields, μ) = @inbounds - μ * fields.u[i, j, 1]
 @inline v_bottom_drag(i, j, grid, clock, fields, μ) = @inbounds - μ * fields.v[i, j, 1]
 
-μ = 1 / 60days
+#μ = 1 / 60days
+μ = 0.0 
+
+# zero boundary conditions 
 # u_bottom_drag_bc = FluxBoundaryCondition(u_bottom_drag, discrete_form=true, parameters=μ)
 u_bottom_drag_bc = ValueBoundaryCondition(0.0)
 
@@ -35,7 +39,11 @@ v_bottom_drag_bc = ValueBoundaryCondition(0.0)
 u_bcs = FieldBoundaryConditions(top=u_bottom_drag_bc, bottom=u_bottom_drag_bc)
 v_bcs = FieldBoundaryConditions(bottom=v_bottom_drag_bc)
 
-νh₀ = 5e3 * (60 / grid.Nx)^2
+#νh₀ = 5e3 * (60 / grid.Nx)^2
+
+# changing this to zero 
+νh₀ = 0.0 
+
 closure = HorizontalScalarDiffusivity(ν = νh₀)
 
 # model = HydrostaticFreeSurfaceModel(; grid, closure,
@@ -54,18 +62,25 @@ model = HydrostaticFreeSurfaceModel(; grid, closure,
                                     tracers = nothing,
                                     buoyancy = nothing)
 
-dmodel = HydrostaticFreeSurfaceModel(; grid, closure,
-                free_surface = ExplicitFreeSurface(),
-                momentum_advection = VectorInvariant(),
-                coriolis = nothing,
-                boundary_conditions = (u=u_bcs, v=v_bcs),
-                tracers = nothing,
-                buoyancy = nothing)
+dmodel = deepcopy(model)
+    
+# HydrostaticFreeSurfaceModel(; grid, closure,
+#                 free_surface = ExplicitFreeSurface(),
+#                 momentum_advection = VectorInvariant(),
+#                 coriolis = HydrostaticSphericalCoriolis(),
+#                 boundary_conditions = (u=u_bcs, v=v_bcs),
+#                 tracers = nothing,
+#                 buoyancy = nothing)
+
+# dmodel = HydrostaticFreeSurfaceModel(; grid, closure)
 
 # Compute stable time-step smaller than grid-scale wave phase propagation
-g = model.free_surface.gravitational_acceleration
+
+#g = model.free_surface.gravitational_acceleration
+g = 0.0
 H = model.grid.Lz
-c = sqrt(g * H) # gravity wave speed
+#c = sqrt(g * H) # gravity wave speed
+c = 0.0
 Δx = minimum(grid.Δxᶜᶜᵃ[1:grid.Nx])
 Δt = 0.1 * Δx / c
 
@@ -91,7 +106,7 @@ v_out = OffsetArray(zeros(66,67,7), -2:63, -2:64, -2:4)
 d_v_out = OffsetArray(ones(66,67,7), -2:63, -2:64, -2:4)
 
 autodiff(ad_func, Duplicated(model, dmodel), Duplicated(u.data, d_u_in), 
-    Duplicated(v.data, d_v_in), 
-    Duplicated(u_out, d_u_out), 
-    Duplicated(v_out, d_v_out))
+          Duplicated(v.data, d_v_in), 
+          Duplicated(u_out, d_u_out), 
+          Duplicated(v_out, d_v_out))
 
