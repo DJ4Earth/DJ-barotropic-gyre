6e3a682f5beb0606caf780650127dddbd4b47cf7
Sarah Williamson <swilliamson@utexas.edu>
2023-05-17 14:54:38 -0500

    Reverting to older version of the code

--- explicit_solver/advance.jl
+++ explicit_solver/advance.jl
@@ -16,7 +16,11 @@ function advance(u_v_eta::gyre_vector,
     ) 
 
     nx = grid.nx 
-    (;dt, rk_a, rk_b) = params
+    dt = params.dt
+
+    # we now use RK4 as the timestepper, here I'm storing the coefficients needed for this 
+    rk_a = [1/6, 1/3, 1/3, 1/6]
+    rk_b = [1/2, 1/2, 1.]
 
     rhs.umid .= u_v_eta.u
     rhs.vmid .= u_v_eta.v
@@ -30,7 +34,7 @@ function advance(u_v_eta::gyre_vector,
     rhs.v1 .= u_v_eta.v
     rhs.eta1 .= u_v_eta.eta
 
-    @inbounds for j in 1:4
+    for j in 1:4
 
         comp_u_v_eta_t(nx, rhs, params, interp, grad, advec)
 
@@ -46,14 +50,9 @@ function advance(u_v_eta::gyre_vector,
 
     end
 
-    # Diffusion and bottom friction as Euler forward
-    dissipative_terms!(nx, rhs, params, interp, grad, advec)
-    rhs.u0 .+= dt .* rhs.u_t
-    rhs.v0 .+= dt .* rhs.v_t
-
-    # @assert all(x -> x < 7.0, rhs.u0)
-    # @assert all(x -> x < 7.0, rhs.v0)
-    # @assert all(x -> x < 7.0, rhs.eta0)
+    @assert all(x -> x < 7.0, rhs.u0)
+    @assert all(x -> x < 7.0, rhs.v0)
+    @assert all(x -> x < 7.0, rhs.eta0)
 
     copyto!(u_v_eta.u, rhs.u0)
     copyto!(u_v_eta.v, rhs.v0)
@@ -92,7 +91,7 @@ function advance(states_rhs::SWM_pde,
 
     for j in 1:4
 
-        comp_u_v_eta_t(nx, states_rhs, params, interp, grad, advec)
+        comp_u_v_eta_t!(nx, states_rhs, params, interp, grad, advec)
 
         if j < 4
             states_rhs.u1 .= states_rhs.umid .+ rk_b[j] .* dt .* states_rhs.u_t
@@ -121,39 +120,39 @@ end
 #           Ly - N-S length of the domain [meters], also has a default 
 # mostly keeping because I like having a function that just integrates for some amount
 # of time 
-function integrate(T, nx, ny; Lx = 3840e3, Ly = 3840e3)                 
+function integrate(days, nx, ny; Lx = 3840e3, Ly = 3840e3)                 
     
-    grid_params = build_grid(Lx, Ly, nx, ny)
-    gyre_params = def_params(grid_params)
-    
-    # building discrete operators 
-    grad_ops = build_derivs(grid_params)                # discrete gradient operators 
-    interp_ops = build_interp(grid_params, grad_ops)    # discrete interpolation operators (travels between grids)
-    advec_ops = build_advec(grid_params)
-    rhs_terms = RHS_terms(Nu = grid_params.Nu, Nv = grid_params.Nv, NT = grid_params.NT, Nq = grid_params.Nq)
-    
-    # starting from rest ---> all initial conditions are zero 
-    
-    Trun = days_to_seconds(T, gyre_params.dt)
-    
-    uout = zeros(grid_params.Nu) 
-    vout = zeros(grid_params.Nv) 
-    etaout = zeros(grid_params.NT)
-    
-    # u = [zeros(grid_params.Nu)]
-    # v = [zeros(grid_params.Nv)]
-    # eta = [zeros(grid_params.NT)]
+    grid = build_grid(Lx, Ly, nx, ny)
+    params = def_params(grid)
 
-    u_v_eta = gyre_vector(uout, vout, etaout)
+    # building discrete operators
+    grad = build_derivs(grid)            # discrete gradient operators
+    interp = build_interp(grid, grad)    # discrete interpolation operators (travels between grids)
+    advec = build_advec(grid)
+
+    Nu = grid.Nu
+    Nv = grid.Nv
+    NT = grid.NT
+    Nq = grid.Nq 
+
+    T = days_to_seconds(days, params.dt)
+
+    states_rhs = SWM_pde(Nu = Nu, 
+        Nv = Nv,
+        NT = NT, 
+        Nq = Nq, 
+        T = T
+    )
     
-    @time for t in 1:Trun
-        advance(u_v_eta, grid_params, rhs_terms, gyre_params, interp_ops, grad_ops, advec_ops)
+    @time for t in 1:T
+        advance!(states_rhs, grid, params, interp, grad, advec)
+        copyto!(states_rhs.u, states_rhs.u0)
+        copyto!(states_rhs.v, states_rhs.v0)
+        copyto!(states_rhs.eta, states_rhs.eta0)
     end
+        
+    return states_rhs
     
-    # u_v_eta_mat = vec_to_mat(u_v_eta.u, u_v_eta.v, u_v_eta.eta, grid_params)
-    
-    return u_v_eta
-    # return (u_v_eta, grid_params, rhs_terms, gyre_params, interp_ops, grad_ops, advec_ops)
 end
 
 # ****IMPORTANT**** not yet sure if I'm moving between high and low res grids, need to check with Patrick
--- explicit_solver/compute_time_deriv.jl
+++ explicit_solver/compute_time_deriv.jl
@@ -2,131 +2,9 @@
 # computes the advection term (needed for the time derivatives). Two versions of each function
 # are defined, each just depends on what type of structure I'm passing for RHS 
 
-function comp_u_v_eta_t(nx::Int, 
+function comp_u_v_eta_t!(
+        nx::Int, 
         rhs::RHS_terms, 
-        params::Params, 
-        interp::Interps, 
-        grad::Derivatives, 
-        advec::Advection
-    )
-
-    # unpack stuff
-    u = rhs.u1
-    v = rhs.v1
-    eta = rhs.eta1
-
-    (;ITu, ITv, ITq, IuT, IvT) = interp                 # interpolation operators
-    (;Iuv, Ivu, Iqu, Iqv) = interp       
-    (;GTx, GTy, Gux, Guy, Gvx, Gvy) = grad              # gradient operators
-    (;h, h_u, h_v, h_q, U, V, p, q) = rhs               # diagnostic variables
-    (;kinetic) = rhs
-    (;GTx_p, GTy_p, Gux_U, Gvy_V, Gvx_v1, Guy_u1) = rhs
-    (;adv_u, adv_v) = rhs
-    (;IuT_u1, IvT_v1, ITu_ksq,ITv_ksq) = rhs
-    (;u_t, v_t, eta_t) = rhs                            # tendencies
-    (;H, coriolis, g, wind_stress) = params
-
-    h .= eta .+ H 
-    
-    @inplacemul h_q = ITq * h
-    @inplacemul h_u = ITu * h
-    @inplacemul h_v = ITv * h
-    U .= u .* h_u                  # volume fluxes U,V
-    V .= v .* h_v 
-    
-    # kinetic energy u² + v²
-    u²_T, u² = IuT_u1, ITu_ksq      # reuse and rename arrays for u²
-    v²_T, v² = IvT_v1, ITv_ksq      # and v², _T is on T-grid
-    u² .= u.^2
-    v² .= v.^2
-    @inplacemul u²_T = IuT * u² 
-    @inplacemul v²_T = IvT * v²
-    kinetic .= u²_T .+ v²_T
-    
-    # Kloewer defined new terms q and p corresponding to potential vorticity and 
-    # Bernoulli potential respectively. To avoid errors in my mimic I'm following 
-    # along and doing the same 
-    @inplacemul Guy_u1 = Guy * u
-    @inplacemul Gvx_v1 = Gvx * v
-    
-    q .= (coriolis .+ Gvx_v1 .- Guy_u1) ./ h_q 
-    p .= 0.5 .* kinetic .+ g .* h
-
-    # deal with the advection term 
-    # comp_advection(nx, rhs, advec)    # Arakawa and Lamb advection scheme
-
-    # Sadourny, 1975 enstrophy conserving advection scheme
-    V_u = ITu_ksq                # reuse and rename array
-    @inplacemul V_u = Ivu * V
-    @inplacemul adv_u = Iqu * q  # u-component qhv
-    adv_u .*= V_u
-
-    U_v = ITv_ksq                # reuse and rename array
-    @inplacemul U_v = Iuv * U         
-    # @inplacemul adv_v = Iqv * q  # v-component -qhu
-    adv_v .*= .-U_v
-
-    # bernoulli gradient ∇p = ∇(1/2(u²+v² + gh))
-    @inplacemul GTx_p = GTx * p
-    @inplacemul GTy_p = GTy * p
-
-    # momentum equations
-    u_t .= adv_u .- GTx_p .+ wind_stress ./ h_u
-    v_t .= adv_v .- GTy_p
-
-    # continuity equations
-    @inplacemul Gux_U = Gux * U    # volume flux divergence dUdx + dVdy
-    @inplacemul Gvy_V = Gvy * V
-    @. eta_t = -(Gux_U + Gvy_V)
-
-    return nothing
-end 
-
-function dissipative_terms!(nx::Int, 
-        rhs::RHS_terms, 
-        params::Params, 
-        interp::Interps, 
-        grad::Derivatives, 
-        advec::Advection
-    )
-
-    # unpack stuff
-    u = rhs.u0                          # calculate based on prognostics at
-    v = rhs.v0                          # t + dt of the non-dissipative RHS
-
-    (;ITu, ITv) = interp                # interpolation operators   
-    (;LLu, LLv) = grad                  # gradient operators
-    (;h_u, h_v) = rhs                   # diagnostic variables
-    (;kinetic, kinetic_sq, Mu, Mv, nu_u, nu_v) = rhs
-    (;bfric_u, bfric_v) = rhs
-    (;ITu_ksq,ITv_ksq) = rhs
-    (;u_t, v_t) = rhs                   # tendencies
-    (;nu, bottom_drag) = params
-
-    # bottom friction
-    kinetic_sq .= sqrt.(kinetic)
-    @inplacemul ITu_ksq = ITu * kinetic_sq
-    @inplacemul ITv_ksq = ITv * kinetic_sq
-    bfric_u .= bottom_drag .* ITu_ksq .* u ./ h_u
-    bfric_v .= bottom_drag .* ITv_ksq .* v ./ h_v
-
-    # diffusion term ν∇⁴(u,v)
-    @inplacemul nu_u = ITu * nu
-    @inplacemul nu_v = ITv * nu
-    @inplacemul Mu = LLu * u
-    @inplacemul Mv = LLv * v
-    Mu .*= nu_u
-    Mv .*= nu_v
-
-    # tendencies for bottom friction and diffusion
-    u_t .= .- Mu .- bfric_u
-    v_t .= .- Mv .- bfric_v 
-
-    return nothing
-end
-
-function comp_u_v_eta_t(nx::Int, 
-        rhs::SWM_pde, 
         params::Params, 
         interp::Interps, 
         grad::Derivatives, 
@@ -160,6 +38,55 @@ function comp_u_v_eta_t(nx::Int,
 
     # rhs.Mu .= params.A_h .* (grad.LLu * rhs.u1)
     # rhs.Mv .= params.A_h .* (grad.LLv * rhs.v1) 
+    
+    rhs.Mu .= (interp.ITu * params.nu) .* (grad.LLu * rhs.u1)
+    rhs.Mv .= (interp.ITv * params.nu) .* (grad.LLv * rhs.v1) 
+
+    rhs.u_t .= rhs.adv_u .- grad.GTx * rhs.p .+ params.wind_stress ./ rhs.h_u .- rhs.Mu .- rhs.bfric_u
+
+    rhs.v_t .= rhs.adv_v .- grad.GTy * rhs.p .- rhs.Mv .- rhs.bfric_v 
+
+    rhs.eta_t .= - (grad.Gux * rhs.U .+ grad.Gvy * rhs.V)
+
+    return nothing
+
+end 
+
+function comp_u_v_eta_t!(nx::Int, 
+        rhs::SWM_pde, 
+        params::Params, 
+        interp::Interps, 
+        grad::Derivatives, 
+        advec::Advection
+    ) 
+
+    rhs.h .= rhs.eta1 .+ params.H 
+
+    rhs.h_u .= interp.ITu * rhs.h
+    rhs.h_v .= interp.ITv * rhs.h
+    rhs.h_q .= interp.ITq * rhs.h
+
+    rhs.U .= rhs.u1 .* rhs.h_u 
+    rhs.V .= rhs.v1 .* rhs.h_v 
+
+    rhs.kinetic .= interp.IuT * (rhs.u1.^2) .+ interp.IvT * (rhs.v1.^2)
+
+    # Kloewer defined new terms q and p corresponding to potential vorticity and 
+    # Bernoulli potential respectively. To avoid errors in my mimic I'm following 
+    # along and doing the same 
+    rhs.q .= (params.coriolis .+ grad.Gvx * rhs.v1 .- grad.Guy * rhs.u1) ./ rhs.h_q 
+    rhs.p .= 0.5 .* rhs.kinetic .+ params.g .* rhs.h
+
+    # bottom friction
+    rhs.kinetic_sq .= sqrt.(rhs.kinetic)
+    rhs.bfric_u .= params.bottom_drag .* ((interp.ITu * rhs.kinetic_sq) .* rhs.u1) ./ rhs.h_u
+    rhs.bfric_v .= params.bottom_drag .* ((interp.ITv * rhs.kinetic_sq) .* rhs.v1) ./ rhs.h_v
+
+    # deal with the advection term 
+    comp_advection!(nx, rhs, advec)
+
+    # rhs.Mu .= params.A_h .* (grad.LLu * rhs.u1)
+    # rhs.Mv .= params.A_h .* (grad.LLv * rhs.v1) 
 
     rhs.Mu .= (interp.ITu * params.nu) .* (grad.LLu * rhs.u1)
     rhs.Mv .= (interp.ITv * params.nu) .* (grad.LLv * rhs.v1) 
@@ -174,7 +101,11 @@ function comp_u_v_eta_t(nx::Int,
 
 end 
 
-function comp_advection(nx::Int, rhs::SWM_pde, advec::Advection)
+function comp_advection!(
+    nx::Int, 
+    rhs::SWM_pde, 
+    advec::Advection
+)
 
     rhs.AL1q .= advec.AL1 * rhs.q 
     rhs.AL2q .= advec.AL2 * rhs.q 
